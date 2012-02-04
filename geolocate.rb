396fd5d33a414636ee0da45056a106af4524f188
Diogo Monica <diogo@squareup.com>
2012-02-04 13:47:11 -0800

    Added configure production for newrelic

--- geolocate.rb
+++ geolocate.rb
@@ -9,6 +9,12 @@ configure do
   GEOIP = GeoIP.new('GeoLiteCity.dat')
 end
 
+configure :production do
+  ENV['APP_ROOT'] ||= File.dirname(__FILE__)
+  $:.unshift "#{ENV['APP_ROOT']}/vendor/plugins/newrelic_rpm/lib"
+  require 'newrelic_rpm'
+end
+
 get '/locate' do
   response['Access-Control-Allow-Origin'] = "*"
   returnable = {:message => "you didn't supply an IP to geocode!"}
